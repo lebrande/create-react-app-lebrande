6ad63dc730b4cfc724a628d948a6b4f13a11537d
Jakub Pusiak <jakub.pusiak@gmail.com>
2020-02-21 00:44:10 +0100

    Add router.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "./App.css";
+import Home from "./components/Home";
+import Tools from "./components/Tools";
+import Profile from "./components/Profile";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Router>
+          <div>
+            <ul className="App-menu">
+              <li className="App-menu-item">
+                <Link to="/">Home</Link>
+              </li>
+              <li className="App-menu-item">
+                <Link to="/tools">About</Link>
+              </li>
+              <li className="App-menu-item">
+                <Link to="/profile">Dashboard</Link>
+              </li>
+            </ul>
+
+            <hr />
+
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route path="/tools">
+                <Tools />
+              </Route>
+              <Route path="/profile">
+                <Profile />
+              </Route>
+            </Switch>
+          </div>
+        </Router>
       </header>
     </div>
   );
---
+++ src/components/Tools.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Tools = () => {
+  return (
+    <ul>
+      <li>map</li>
+      <li>reduce</li>
+      <li>filter</li>
+      <li>find</li>
+      <li>some</li>
+    </ul>
+  );
+};
+
+export default Tools;
